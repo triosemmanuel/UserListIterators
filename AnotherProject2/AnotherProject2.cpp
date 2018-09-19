b36ccb08c86b7fd46a5f004b811606edb163624b
Emmanuel <emmanuel.devries@trios.com>
2018-09-19 16:17:59 -0400

    Should have done this earlier. Added User class

--- AnotherProject2/AnotherProject2.cpp
+++ AnotherProject2/AnotherProject2.cpp
@@ -1,16 +1,54 @@
 // AnotherProject2.cpp : Defines the entry point for the console application.
 //
 
-#include "pch.h"
+#include "stdafx.h"
 
 #include <iostream>
+#include <list>
+
+using namespace std;
+
+
+class User
+{
+	string Username;
+	string Password;
+
+public:
+
+	User(string username, string password)
+	{
+		Username = username;
+		Password = password;
+	}
+
+	string getUsername() // Read only!!!
+	{
+		return Username;
+	}
+
+	string getPassword()
+	{
+		return Password;
+	}
+
+	void setPassword(string password)
+	{
+		Password = password;
+
+	}
+};
 
 
 int main()
 {
 
+
 	std::cout << "hey." << std::endl;
 
+	User my_user = User("con", "brew");
+
+	my_user.setPassword("newpass111");
 
     return 0;
 }
