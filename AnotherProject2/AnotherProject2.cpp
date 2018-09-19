ad18b5e1dee20a9789f008fb7419673aa47e7f5c
Emmanuel <emmanuel.devries@trios.com>
2018-09-19 16:39:43 -0400

    Added the user_list - with 3 test users - and for loop printing every username

--- AnotherProject2/AnotherProject2.cpp
+++ AnotherProject2/AnotherProject2.cpp
@@ -3,6 +3,7 @@
 
 #include "stdafx.h"
 
+#include <string>
 #include <iostream>
 #include <list>
 
@@ -42,14 +43,32 @@ public:
 
 int main()
 {
-
-
-	std::cout << "hey." << std::endl;
+	list<User> user_list = list<User>();
 
 	User my_user = User("con", "brew");
+	user_list.push_back(my_user);
 
-	my_user.setPassword("newpass111");
+	my_user = User("vik", "mur");
+	user_list.push_back(my_user);
 
+	my_user = User("alex", "tav00");
+	user_list.push_back(my_user);
+	
+	/*
+	int i;
+	for (i = 0; i < userCount; i++)
+	{
+
+	}*/
+
+	list<User>::iterator it;
+	for (it = user_list.begin(); it != user_list.end(); it++)
+	{
+		cout << (*it).getUsername() << endl;
+	}
+
+
+	system("PAUSE");
     return 0;
 }
 
